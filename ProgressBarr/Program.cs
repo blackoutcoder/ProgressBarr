eab4c91a804279404f0aa483d52402c23f16c9bc
blackoutcoder <94862044+blackoutcoder@users.noreply.github.com>
2022-05-16 19:51:17 +0300

    Add project files.

---
+++ ProgressBarr/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProgressBarr
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ProgressBar progressBarr = new ();
+            WriteProgress(progressBarr);
+            for (int i = 0; i < 100; i++)
+            {
+                await Task.Delay(1000);
+                progressBarr.Progress++;
+                
+            }
+        }
+
+        static async Task WriteProgress(ProgressBar progressBar)
+        {
+            while(true)
+            {
+                Console.Clear();
+                Console.Write(progressBar.Progress);
+                await Task.Delay(3000);
+            }
+        }
+    }
+}
